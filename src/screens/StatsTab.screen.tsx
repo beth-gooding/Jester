194fc344b0213acbc4b845070ecaf21f872e56b9
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-24 18:01:29 +0100

    Changes screen file names

---
+++ src/screens/StatsTab.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function StatsTab() {
+    return (
+        <View>
+            <Text>Stats screens</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
