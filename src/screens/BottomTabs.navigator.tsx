64dd42263dba5b6b8779145636fd50c3f8935d78
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-25 15:16:29 +0100

    Adds icons for all tabs

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -5,6 +5,9 @@ import { NewJokesTab } from './NewJokesTab.screen';
 import { SavedJokesTab } from './SavedJokesTab.screen';
 import { StatsTab } from './StatsTab.screen';
 import { AddJokeIcon } from '../components/AddJoke.icon';
+import { SavedJokeIcon } from '../components/SavedJokes.icon';
+import { StatsIcon } from '../components/Stats.icon';
+
 const Tab = createBottomTabNavigator();
 
 export const BottomTabs: React.FC = () => {
@@ -18,9 +21,14 @@ export const BottomTabs: React.FC = () => {
                 tabBarIcon: ({ color, size }) => {
                 if (route.name === 'NewJokes') {
                     return <AddJokeIcon size={size} color={color}/>
-                } else {
-                return <Text>{route.name}</Text>
+                } 
+                if (route.name === 'SavedJokes') {
+                    return <SavedJokeIcon size={size} color={color}/>
+                } 
+                if (route.name === 'Stats') {
+                    return <StatsIcon size={size} color={color}/>
                 }
+                return null;
             }}
 
         }}>
