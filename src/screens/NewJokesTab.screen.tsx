89afc6f0ac9b92ff61433f199a99c8e322788748
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-25 10:24:32 +0100

    Fetches jokes on app loading, and fetches new joke when you click discard

--- src/screens/NewJokesTab.screen.tsx
+++ src/screens/NewJokesTab.screen.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useCallback, useState, useEffect } from 'react';
 
 export const NewJokesTab: React.FC = () => {
+    const [joke, setJoke] = useState();
+    const handleFetchNewJoke = useCallback(async () => {
+        const res = await fetch('https://icanhazdadjoke.com/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'User-Agent': 'My Library (https://github.com/beth-gooding/JokeJenerator)'
+            }
+        })
+        const newJoke = await res.json();
+        if (res.ok) {
+            setJoke(newJoke);
+        }
+    }, [])
+    useEffect(() => {
+        handleFetchNewJoke();
+    }, [handleFetchNewJoke])
+
     return (
         <View style={styles.jokeJenerator}>
-            <Text>New Jokes screen</Text>
+            <Text style={styles.joke}>{joke.joke}</Text>
+            <View style={styles.btnContainer}>
+            <TouchableOpacity>
+                <Text>Save</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleFetchNewJoke}>
+                <Text>Discard</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -13,6 +41,13 @@ const styles = StyleSheet.create({
     jokeJenerator: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
       },
+    joke: {
+        textAlign: 'center',
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingTop: 15,
+    },
 })
