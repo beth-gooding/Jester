4eda2879648756b3d277e0e888dda2117e638620
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-25 08:58:15 +0100

    Configures TypeScript

--- src/screens/StatsTab.screen.tsx
+++ src/screens/StatsTab.screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function StatsTab() {
+export const StatsTab: React.FC = () => {
     return (
         <View>
             <Text>Stats screens</Text>
