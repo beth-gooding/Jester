adabdb221583985c058aed32bbac63c2923d45d0
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-26 16:17:09 +0100

    Adds swipe to delete to the joke items

--- src/components/SavedJokeListItem.tsx
+++ src/components/SavedJokeListItem.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  LayoutAnimation,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import { JokeWithTimeStamp } from '../types';
 import { DiscardJokeIcon } from '../components/DiscardJoke.icon';
 import { useAppContext } from '../App.provider';
 import {
   PanGestureHandler,
   PanGestureHandlerGestureEvent,
+  PanGestureHandlerStateChangeEvent,
+  State as GestureState,
 } from 'react-native-gesture-handler';
 import { useCallback } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 type JokeItemRowProps = {
   jokeObject: JokeWithTimeStamp;
@@ -15,21 +27,59 @@ type JokeItemRowProps = {
 
 export const SavedJokeListItem: React.FC<JokeItemRowProps> = (jokeItem) => {
   const { handleDeleteJoke } = useAppContext();
-  const onGestureEvent = useCallback((event: PanGestureHandlerGestureEvent) => {
-    console.warn(event.nativeEvent.translationX);
-  }, []);
+
+  const offset = useSharedValue(0);
+  const maxPan = 100;
+  const [shouldDelete, setShouldDelete] = useState(false);
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+
+  const onGestureEvent = useCallback(
+    (event: PanGestureHandlerGestureEvent) => {
+      const xVal = Math.floor(event.nativeEvent.translationX);
+      offset.value = xVal;
+
+      if (Math.abs(xVal) <= maxPan) {
+        setShouldDelete(false);
+      } else {
+        setShouldDelete(true);
+      }
+    },
+    [offset],
+  );
+
+  const onHandlerStateChange = useCallback(
+    (event: PanGestureHandlerStateChangeEvent) => {
+      if (event.nativeEvent.state === GestureState.END) {
+        if (shouldDelete) {
+          offset.value = withTiming(Math.sign(offset.value) * 5000);
+          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+          setTimeout(() => {
+            handleDeleteJoke(jokeItem.jokeObject);
+          }, 250);
+        } else {
+          offset.value = withTiming(0);
+        }
+      }
+    },
+    [handleDeleteJoke, offset, jokeItem, shouldDelete],
+  );
+
   return (
     <PanGestureHandler
       minDeltaX={1}
       minDeltaY={100}
       onGestureEvent={onGestureEvent}
+      onHandlerStateChange={onHandlerStateChange}
     >
-      <View style={styles.jokeContainer}>
+      <Animated.View style={[styles.jokeContainer, animatedStyles]}>
         <Text style={styles.joke}>{jokeItem.jokeObject.joke}</Text>
         <TouchableOpacity onPress={() => handleDeleteJoke(jokeItem.jokeObject)}>
           <DiscardJokeIcon size={30} color={'red'} />
         </TouchableOpacity>
-      </View>
+      </Animated.View>
     </PanGestureHandler>
   );
 };
