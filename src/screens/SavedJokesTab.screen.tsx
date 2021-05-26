9479b5609bdd6518c381ef2b8667f93b7adcf763
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-26 09:46:01 +0100

    Refactors SavedJokesTab to use a component for the list items

--- src/screens/SavedJokesTab.screen.tsx
+++ src/screens/SavedJokesTab.screen.tsx
@@ -1,45 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { useAppContext } from '../App.provider';
-import { format } from 'date-fns';
+import { SavedJokeListItem } from '../components/SavedJokeListItem';
 
 export const SavedJokesTab: React.FC = () => {
-
-    const { savedJokes } = useAppContext();
-    return (
-        <FlatList 
-          style={styles.listContainer}
-          keyExtractor={item => item.timestamp}
-          data={savedJokes}
-          renderItem={({ item }) => (
-              <View style={styles.jokeContainer} >
-                  <Text style={styles.joke}>{item.joke}</Text>
-                  <Text>{format(new Date(item.timestamp), 'dd MMM, yyyy')}</Text>
-              </View>
-          )} />
-    )
-}
+  const { savedJokes } = useAppContext();
+  return (
+    <FlatList
+      style={styles.listContainer}
+      keyExtractor={(item) => item.timestamp}
+      data={savedJokes}
+      renderItem={({ item }) => <SavedJokeListItem jokeObject={item} />}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    listContainer: {
-        backgroundColor: 'white',
-    },
-    jokeContainer: {
-        backgroundColor: 'white',
-        borderWidth: 2,
-        borderRadius: 15,
-        margin: 5,
-        padding: 5
-    },
-    joke: {
-        textAlign: 'center',
-        paddingVertical: 5,
-        fontWeight: 'bold',
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContainer: {
+    backgroundColor: 'white',
+  },
+  jokeContainer: {
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderRadius: 15,
+    margin: 5,
+    padding: 5,
+  },
+  joke: {
+    textAlign: 'center',
+    paddingVertical: 5,
+    fontWeight: 'bold',
+  },
+});
