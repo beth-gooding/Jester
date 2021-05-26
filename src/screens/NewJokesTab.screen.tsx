3df40acad13444d2dfde15402f5bbd539e583458
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-26 11:12:15 +0100

    Adds speech bubble styling

--- src/screens/NewJokesTab.screen.tsx
+++ src/screens/NewJokesTab.screen.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 import { useEffect } from 'react';
 import { useAppContext } from '../App.provider';
 import { StarJokeIcon } from '../components/StarJoke.icon';
 import { DiscardJokeIcon } from '../components/DiscardJoke.icon';
+
+const imageSrc = require('../assets/images/chat.png');
 
 export const NewJokesTab: React.FC = () => {
   const { joke } = useAppContext();
@@ -16,18 +24,20 @@ export const NewJokesTab: React.FC = () => {
 
   return (
     <View style={styles.jokeJenerator}>
-      <View style={styles.jokeContainer}>
-        <Text style={styles.jokeTitle}>Here's a joke for you:</Text>
-        <Text style={styles.joke}>{joke}</Text>
-      </View>
-      <View style={styles.btnContainer}>
-        <TouchableOpacity onPress={() => handleSave(joke)}>
-          <StarJokeIcon />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleFetchNewJoke}>
-          <DiscardJokeIcon />
-        </TouchableOpacity>
-      </View>
+      <ImageBackground source={imageSrc} style={styles.speechBubble}>
+        <View style={styles.jokeContainer}>
+          <Text style={styles.jokeTitle}>Here's a joke for you:</Text>
+          <Text style={styles.joke}>{joke}</Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity onPress={() => handleSave(joke)}>
+            <StarJokeIcon />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleFetchNewJoke}>
+            <DiscardJokeIcon />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -36,27 +46,31 @@ const styles = StyleSheet.create({
   jokeJenerator: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#1C72E3',
   },
   joke: {
     textAlign: 'center',
     padding: 5,
-    fontWeight: 'bold',
-    fontSize: 16,
+    fontSize: 22,
   },
   jokeTitle: {
     textAlign: 'center',
     padding: 5,
     fontWeight: 'bold',
-    fontSize: 20,
+    fontSize: 18,
   },
   btnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    paddingTop: 15,
+    paddingBottom: 80,
   },
   jokeContainer: {
-    borderWidth: 2,
-    borderRadius: 20,
-    marginHorizontal: 5,
+    margin: 5,
+  },
+  speechBubble: {
+    height: 350,
+    aspectRatio: 1,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
   },
 });
