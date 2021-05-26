bcbb08f4c445377ceb71a8d078f0214318aa7e5f
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-26 08:54:48 +0100

    Applies linting and prettier

--- src/screens/NewJokesTab.screen.tsx
+++ src/screens/NewJokesTab.screen.tsx
@@ -6,58 +6,57 @@ import { StarJokeIcon } from '../components/StarJoke.icon';
 import { DiscardJokeIcon } from '../components/DiscardJoke.icon';
 
 export const NewJokesTab: React.FC = () => {
-    const { joke } = useAppContext();
-    const { handleFetchNewJoke } = useAppContext();
-    const { handleSave } = useAppContext();
+  const { joke } = useAppContext();
+  const { handleFetchNewJoke } = useAppContext();
+  const { handleSave } = useAppContext();
 
-    useEffect(() => {
-        handleFetchNewJoke();
-    }, [handleFetchNewJoke])
+  useEffect(() => {
+    handleFetchNewJoke();
+  }, [handleFetchNewJoke]);
 
-    return (
-        <View style={styles.jokeJenerator}>
-            <View style={styles.jokeContainer} >
-                <Text style={styles.jokeTitle}>Here's a joke for you:</Text>
-                <Text style={styles.joke}>{joke}</Text>
-            </View>
-            <View style={styles.btnContainer}>
-            <TouchableOpacity onPress={handleSave}>
-                <StarJokeIcon />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleFetchNewJoke}>
-                <DiscardJokeIcon />
-            </TouchableOpacity>
-            </View>
-            
-        </View>
-    )
-}
+  return (
+    <View style={styles.jokeJenerator}>
+      <View style={styles.jokeContainer}>
+        <Text style={styles.jokeTitle}>Here's a joke for you:</Text>
+        <Text style={styles.joke}>{joke}</Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={handleSave}>
+          <StarJokeIcon />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleFetchNewJoke}>
+          <DiscardJokeIcon />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    jokeJenerator: {
-        flex: 1,
-        justifyContent: 'center',
-      },
-    joke: {
-        textAlign: 'center',
-        padding: 5,
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
-    jokeTitle: {
-        textAlign: 'center',
-        padding: 5,
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-    btnContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        paddingTop: 15,
-    },
-    jokeContainer: {
-        borderWidth: 2,
-        borderRadius: 20,
-        marginHorizontal: 5,
-    },
-})
+  jokeJenerator: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  joke: {
+    textAlign: 'center',
+    padding: 5,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  jokeTitle: {
+    textAlign: 'center',
+    padding: 5,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 15,
+  },
+  jokeContainer: {
+    borderWidth: 2,
+    borderRadius: 20,
+    marginHorizontal: 5,
+  },
+});
