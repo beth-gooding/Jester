15f7823e8c21eef11f1538a6887a1ed82e9f5fee
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-28 11:31:11 +0100

    Adds a piechart

---
+++ src/components/SavedByHour.chart.tsx
@@ -0,0 +1,55 @@
+import React, { useMemo } from 'react';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../App.provider';
+import groupBy from 'lodash/groupBy';
+import orderBy from 'lodash/orderBy';
+import { format } from 'date-fns';
+import { Drawer } from './Drawer';
+import { StyleSheet, View } from 'react-native';
+
+export const SavedByHour: React.FC = () => {
+  const { savedJokes } = useAppContext();
+  const data = useMemo(() => {
+    const orderedJokes = orderBy(savedJokes, 'timestamp', 'asc');
+    const groupedJokes = groupBy(orderedJokes, (item) =>
+      format(new Date(item.timestamp), 'HH'),
+    );
+    return Object.entries(groupedJokes).map(([day, jokesInHour]) => ({
+      x: day,
+      y: jokesInHour.length,
+    }));
+  }, [savedJokes]);
+
+  return (
+    <View style={styles.dayContainer}>
+      <Drawer title={'Overall Number of Jokes \n Saved Per Hour'}>
+        <VictoryPie
+          data={data}
+          width={300}
+          innerRadius={40}
+          colorScale={'blue'}
+        />
+      </Drawer>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  jokeJenerator: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dayContainer: {
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 15,
+    borderColor: '#1C72E3',
+    backgroundColor: 'white',
+    opacity: 0.95,
+  },
+});
--- src/screens/StatsTab.screen.tsx
+++ src/screens/StatsTab.screen.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SavedByDay } from '../components/SavedByDay.chart';
+import { SavedByHour } from '../components/SavedByHour.chart';
+
+const networkImageUrl =
+  'https://images.unsplash.com/photo-1521913626209-0fbf68f4c4b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80';
 
 export const StatsTab: React.FC = () => {
   return (
-    <View style={styles.jokeJenerator}>
+    <ImageBackground
+      source={{ uri: networkImageUrl }}
+      style={styles.jokeJenerator}
+    >
       <View style={styles.container}>
         <ScrollView style={styles.scrollContainer}>
           <SavedByDay />
+          <SavedByHour />
         </ScrollView>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -19,8 +27,6 @@ const styles = StyleSheet.create({
   jokeJenerator: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#febd00',
   },
   container: {
     flex: 1,
@@ -33,6 +39,6 @@ const styles = StyleSheet.create({
     fontSize: 21,
   },
   scrollContainer: {
-    paddingVertical: 15,
+    paddingTop: 15,
   },
 });
