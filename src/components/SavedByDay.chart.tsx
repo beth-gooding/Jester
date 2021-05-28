664cdec23890ff01e6e2fd4a7db4caf0d0320fb7
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-28 10:26:19 +0100

    Refactors graph into a component

---
+++ src/components/SavedByDay.chart.tsx
@@ -0,0 +1,95 @@
+import React, { useMemo } from 'react';
+import { StyleSheet } from 'react-native';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryLabel,
+} from 'victory-native';
+import { useAppContext } from '../App.provider';
+import groupBy from 'lodash/groupBy';
+import orderBy from 'lodash/orderBy';
+import { format } from 'date-fns';
+
+export const SavedByDay: React.FC = () => {
+  const { savedJokes } = useAppContext();
+  const data = useMemo(() => {
+    const orderedJokes = orderBy(savedJokes, 'timestamp', 'asc');
+    const groupedJokes = groupBy(orderedJokes, (item) =>
+      format(new Date(item.timestamp), 'dd.MM'),
+    );
+    return Object.entries(groupedJokes).map(([day, jokesInDay]) => ({
+      x: day,
+      y: jokesInDay.length,
+    }));
+  }, [savedJokes]);
+
+  return (
+    <VictoryChart
+      width={350}
+      height={400}
+      domainPadding={{ x: 15, y: 5 }}
+      style={{ background: { fill: 'white' } }}
+    >
+      <VictoryLabel
+        text={['Number of Jokes', 'Saved each Day']}
+        x={175}
+        y={20}
+        textAnchor="middle"
+        style={styles.title}
+      />
+      <VictoryBar
+        data={data}
+        style={{ data: { fill: '#1C72E3' } }}
+        animate={{
+          duration: 2000,
+          easing: 'bounce',
+          onLoad: { duration: 1000 },
+        }}
+      />
+      <VictoryAxis
+        label="Date"
+        style={{
+          axisLabel: {
+            fontSize: 22,
+            fontFamily: 'TitilliumWeb-Regular',
+            fontWeight: 'bold',
+            padding: 25,
+          },
+
+          tickLabels: {
+            fontSize: 18,
+            padding: 5,
+            fontFamily: 'TitilliumWeb-Light',
+          },
+        }}
+      />
+      <VictoryAxis
+        dependentAxis={true}
+        label="Number of saved jokes"
+        style={{
+          axisLabel: {
+            fontSize: 22,
+            fontFamily: 'TitilliumWeb-Regular',
+            fontWeight: 'bold',
+            padding: 30,
+          },
+          tickLabels: {
+            fontSize: 20,
+            padding: 1,
+            fontFamily: 'TitilliumWeb-Light',
+          },
+          ticks: { stroke: 'black', size: 5 },
+        }}
+      />
+    </VictoryChart>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'TitilliumWeb-Bold',
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+});
--- src/screens/StatsTab.screen.tsx
+++ src/screens/StatsTab.screen.tsx
@@ -1,72 +1,12 @@
-import React, { useMemo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory-native';
-import { useAppContext } from '../App.provider';
-import groupBy from 'lodash/groupBy';
-import orderBy from 'lodash/orderBy';
-import { format } from 'date-fns';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SavedByDay } from '../components/SavedByDay.chart';
 
 export const StatsTab: React.FC = () => {
-  const { savedJokes } = useAppContext();
-  const data = useMemo(() => {
-    const orderedJokes = orderBy(savedJokes, 'timestamp', 'asc');
-    const groupedJokes = groupBy(orderedJokes, (item) =>
-      format(new Date(item.timestamp), 'dd.MM'),
-    );
-    return Object.entries(groupedJokes).map(([day, jokesInDay]) => ({
-      x: day,
-      y: jokesInDay.length,
-    }));
-  }, [savedJokes]);
-
   return (
     <View style={styles.jokeJenerator}>
       <View style={styles.container}>
-        <Text>Some nice graphs</Text>
-        <VictoryChart
-          width={350}
-          height={400}
-          style={{ background: { fill: 'white' } }}
-        >
-          <VictoryBar
-            data={data}
-            alignment="start"
-            style={{ data: { fill: '#1C72E3' } }}
-          />
-          <VictoryAxis
-            label="Date"
-            style={{
-              axisLabel: {
-                fontSize: 25,
-                fontFamily: 'TitilliumWeb-Regular',
-                fontWeight: 'bold',
-              },
-
-              tickLabels: {
-                fontSize: 20,
-                padding: 5,
-                fontFamily: 'TitilliumWeb-Light',
-              },
-            }}
-          />
-          <VictoryAxis
-            dependentAxis={true}
-            label="Number of saved jokes"
-            style={{
-              axisLabel: {
-                fontSize: 20,
-                fontFamily: 'TitilliumWeb-Regular',
-                fontWeight: 'bold',
-              },
-              tickLabels: {
-                fontSize: 20,
-                padding: 5,
-                fontFamily: 'TitilliumWeb-Light',
-              },
-              ticks: { stroke: 'black', size: 5 },
-            }}
-          />
-        </VictoryChart>
+        <SavedByDay />
       </View>
     </View>
   );
@@ -83,5 +23,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'TitilliumWeb-Bold',
+    fontWeight: 'bold',
+    fontSize: 21,
   },
 });
