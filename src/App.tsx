a8682db744e039bb63cf623af07f936169c0b602
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-28 16:01:22 +0100

    Adds custom splashscreen to iOS

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { RootNavigator } from './screens/Root.navigator';
 import { AppProvider } from './App.provider';
 
 import { Platform, UIManager } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -12,6 +13,9 @@ if (Platform.OS === 'android') {
 }
 
 export const App: React.FC = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <AppProvider>
       <NavigationContainer>
