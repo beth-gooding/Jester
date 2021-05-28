d26628879b7fa39d4225d2aff7825a587e42fb8e
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-28 11:35:17 +0100

    Puts barchart in a drawer

--- src/components/SavedByDay.chart.tsx
+++ src/components/SavedByDay.chart.tsx
@@ -1,15 +1,11 @@
 import React, { useMemo } from 'react';
-import { StyleSheet } from 'react-native';
-import {
-  VictoryBar,
-  VictoryChart,
-  VictoryAxis,
-  VictoryLabel,
-} from 'victory-native';
+import { StyleSheet, View } from 'react-native';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory-native';
 import { useAppContext } from '../App.provider';
 import groupBy from 'lodash/groupBy';
 import orderBy from 'lodash/orderBy';
 import { format } from 'date-fns';
+import { Drawer } from './Drawer';
 
 export const SavedByDay: React.FC = () => {
   const { savedJokes } = useAppContext();
@@ -25,64 +21,61 @@ export const SavedByDay: React.FC = () => {
   }, [savedJokes]);
 
   return (
-    <VictoryChart
-      width={350}
-      height={400}
-      domainPadding={{ x: 15, y: 5 }}
-      style={{ background: { fill: 'white' } }}
-    >
-      <VictoryLabel
-        text={['Number of Jokes', 'Saved each Day']}
-        x={175}
-        y={20}
-        textAnchor="middle"
-        style={styles.title}
-      />
-      <VictoryBar
-        data={data}
-        style={{ data: { fill: '#1C72E3' } }}
-        animate={{
-          duration: 2000,
-          easing: 'bounce',
-          onLoad: { duration: 1000 },
-        }}
-      />
-      <VictoryAxis
-        label="Date"
-        style={{
-          axisLabel: {
-            fontSize: 22,
-            fontFamily: 'TitilliumWeb-Regular',
-            fontWeight: 'bold',
-            padding: 25,
-          },
+    <View style={styles.dayContainer}>
+      <Drawer title={'Number of Jokes Saved Each Day'}>
+        <VictoryChart
+          width={350}
+          height={400}
+          domainPadding={{ x: 15, y: 5 }}
+          style={{ background: { fill: 'white' } }}
+        >
+          <VictoryBar
+            data={data}
+            style={{ data: { fill: '#1C72E3' } }}
+            animate={{
+              duration: 2000,
+              easing: 'bounce',
+              onLoad: { duration: 1000 },
+            }}
+          />
+          <VictoryAxis
+            label="Date"
+            style={{
+              axisLabel: {
+                fontSize: 22,
+                fontFamily: 'TitilliumWeb-Regular',
+                fontWeight: 'bold',
+                padding: 25,
+              },
 
-          tickLabels: {
-            fontSize: 18,
-            padding: 5,
-            fontFamily: 'TitilliumWeb-Light',
-          },
-        }}
-      />
-      <VictoryAxis
-        dependentAxis={true}
-        label="Number of saved jokes"
-        style={{
-          axisLabel: {
-            fontSize: 22,
-            fontFamily: 'TitilliumWeb-Regular',
-            fontWeight: 'bold',
-            padding: 30,
-          },
-          tickLabels: {
-            fontSize: 20,
-            padding: 1,
-            fontFamily: 'TitilliumWeb-Light',
-          },
-          ticks: { stroke: 'black', size: 5 },
-        }}
-      />
-    </VictoryChart>
+              tickLabels: {
+                fontSize: 18,
+                padding: 5,
+                fontFamily: 'TitilliumWeb-Light',
+              },
+            }}
+          />
+          <VictoryAxis
+            dependentAxis={true}
+            label="Number of saved jokes"
+            style={{
+              axisLabel: {
+                fontSize: 22,
+                fontFamily: 'TitilliumWeb-Regular',
+                fontWeight: 'bold',
+                padding: 30,
+              },
+              tickLabels: {
+                fontSize: 20,
+                padding: 1,
+                fontFamily: 'TitilliumWeb-Light',
+              },
+              ticks: { stroke: 'black', size: 5 },
+            }}
+          />
+        </VictoryChart>
+      </Drawer>
+    </View>
   );
 };
 
@@ -91,5 +84,13 @@ const styles = StyleSheet.create({
     fontFamily: 'TitilliumWeb-Bold',
     fontWeight: 'bold',
     fontSize: 21,
+  },
+  dayContainer: {
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 15,
+    borderColor: '#1C72E3',
+    backgroundColor: 'white',
+    opacity: 0.95,
   },
 });
