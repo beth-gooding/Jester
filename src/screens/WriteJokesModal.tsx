c28761037a6c24ef124604605edfa5dd76542bb2
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-06-14 17:17:28 +0100

    Adds form input and form submit button

--- src/screens/WriteJokesModal.tsx
+++ src/screens/WriteJokesModal.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { IconButton } from '../components/IconButton';
 
 export const WriteJokesModal: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>This is my modal where the user can write new jokes</Text>
+      <TextInput style={styles.input} multiline={true} />
+      <IconButton onPressFunction={() => null} iconStyles={[styles.submitBtn]}>
+        <Text>Submit your joke!</Text>
+      </IconButton>
     </View>
   );
 };
@@ -14,5 +19,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#1C72E3',
+    padding: 5,
+    marginVertical: 10,
+    borderRadius: 10,
+    width: 300,
+  },
+  submitBtn: {
+    borderColor: '#1C72E3',
+    borderWidth: 2,
   },
 });
