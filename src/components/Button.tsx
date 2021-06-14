7a7d2816e4883f2b25bc33df8bf44170d9de6309
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-06-14 11:08:47 +0100

    Imports the text element from the correct place

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'victory-core';
+import { Text } from 'react-native';
 
 export const Button: React.FC = () => {
   return <Text>This is a button</Text>;
